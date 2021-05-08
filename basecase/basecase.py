c71b66885fe2bac6b937cd4ed2c3f34d7e540be0
cheng2020-G <1007884377@qq.com>
2021-05-08 16:35:51 +0800

    alter basecase

--- basecase/basecase.py
+++ basecase/basecase.py
@@ -4,6 +4,6 @@ from api.GetToken import Token
 
 class BaseCase:
     def setup(self):
-        # 实例化getUserid接口
+        # 实例化接口
         self.GetUserId = GetUserId()
         self.GetToken = Token()
