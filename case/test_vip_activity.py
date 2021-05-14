17c8f9126a962a7b1ac3b7ffa407978c1f907714
cheng2020-G <1007884377@qq.com>
2021-05-14 17:12:46 +0800

    增加assert类型：正则匹配和json断言

--- case/test_vip_activity.py
+++ case/test_vip_activity.py
@@ -1,3 +1,5 @@
+import re
+
 from basecase.basecase import BaseCase
 
 
@@ -6,3 +8,15 @@ class TestVipActivity(BaseCase):
         res = self.vipactivity.vip_activity()
         print(res.json())
         assert res.status_code == 200
+        assert res.json()['retCode'] == 0
+        assert res.json()['data']['id'] == 717
+        assert res.json()['data']['isFollow'] == 1
+        assert res.json()['data']['rechargeType'] == 0
+        assert re.search('\w+', res.json()['data']['name'])
+        assert re.search('#([0-9a-fA-F]{6})', res.json()['data']['bgColor'])
+        assert re.search('#([0-9a-fA-F]{6})', res.json()['data']['fontColor'])
+        assert re.search('\w+', res.json()['data']['rule'])
+        assert re.search('http://\S+', res.json()['data']['imageOne'])
+        assert re.search('http://\S+', res.json()['data']['imageTwo'])
+        assert re.search('\d{4}.\d{2}.\d{2}.\s\d{2}:\d{2}', res.json()['data']['beginTime'])
+        assert re.search('\d{4}.\d{2}.\d{2}.\s\d{2}:\d{2}', res.json()['data']['endTime'])
