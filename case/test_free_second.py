088886675d5b6f9399ad211add0d1ed17609ee37
cheng2020-G <1007884377@qq.com>
2021-05-26 11:20:11 +0800

    断言：遍历list

--- case/test_free_second.py
+++ case/test_free_second.py
@@ -12,21 +12,14 @@ class TestFreeSecond(BaseCase):
         assert res.status_code == 200
         assert res.json()['retCode'] == 0
         assert re.search(r'\d', str(res.json()['data']['page']))
-        for author in res.json()['data']['section']:
-            print(author)
-            assert re.search(r'\w+', str(author['author']))
-        for coverWap in res.json()['data']['section']:
-            assert re.search(r'http://\w+', str(coverWap['coverWap']))
-        for clickNum in res.json()['data']['section']:
-            assert re.search(r'\w+', str(clickNum['clickNum']))
-        for totalWordSize in res.json()['data']['section']:
-            assert re.search(r'\w+', str(totalWordSize['totalWordSize']))
-        for bookName in res.json()['data']['section']:
-            assert re.search(r'\w+', str(bookName['bookName']))
-        for introduction in res.json()['data']['section']:
-            assert re.search(r'\w+', str(introduction['introduction']))
-        for bookId in res.json()['data']['section']:
-            assert re.search(r'\d+', str(bookId['bookId']))
-        for status in res.json()['data']['section']:
-            assert re.search(r'\w+', str(status['status']))
+        for i in res.json()['data']['section']:
+            print(i)
+            assert re.search(r'\w+', str(i['author']))
+            assert re.search(r'http://\w+', str(i['coverWap']))
+            assert re.search(r'\w+', str(i['clickNum']))
+            assert re.search(r'\w+', str(i['totalWordSize']))
+            assert re.search(r'\w+', str(i['bookName']))
+            assert re.search(r'\w+', str(i['introduction']))
+            assert re.search(r'\d+', str(i['bookId']))
+            assert re.search(r'\w+', str(i['status']))
         # assert re.search(r'\d+', str((for bookId in res.json()['data']['section']: return)['bookId'])
