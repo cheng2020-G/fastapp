c543a0d289e52e98cc510e32969fd5c71b89af8c
cheng2020-G <1007884377@qq.com>
2021-05-27 09:54:42 +0800

    类优化

--- api/login.py
+++ api/login.py
@@ -2,11 +2,12 @@ import yaml
 from base.baseapi import BaseApi
 
 
-class LogIn(object):
+class LogIn:
     # 封装login接口方法，case调用此方法，data从case层传入
     def login(self, data_login):
         self.http = BaseApi()
         return self.http.http(data_login)
+
 
 # if __name__ == '__main__':
 #     data_login_yaml = yaml.safe_load(open('D:/script/fastapp/data/login.yaml'))[0]
--- case/test_login.py
+++ case/test_login.py
@@ -5,7 +5,7 @@ from basecase.basecase import BaseCase
 import re
 
 
-class TestLogIN():
+class TestLogIN:
     # def test_get_code(self):
     #     res = self.get_code.get_code()
     #     print('请求url：' + res.url)
@@ -37,13 +37,32 @@ class TestLogIN():
         data = yaml.safe_load(open('D:/script/fastapp/data/get_code.yaml'))
         res = LogIn().login(data)
         print(res.json())
+        assert res.status_code == 200
+        assert res.json()['retCode'] == 0
+        assert res.json()['isExpire'] == 1
+        assert re.search('您的安全验证码是：', res.json()['data']['mtKeyword'])
+        assert re.search(r'.+?', res.json()['data']['rule'])
+        assert res.json()['data']['result'] == '1'
 
     def test_login_success(self):
         data = yaml.safe_load(open('D:/script/fastapp/data/login.yaml'))[0]
         res = LogIn().login(data)
         print(res.json())
+        assert res.status_code == 200
+        assert res.json()['retCode'] == 0
+        assert res.json()['isExpire'] == 1
+        assert res.json()['data']['code'] == 0
+        assert res.json()['data']['message'] == '登录成功'
+        assert res.json()['data']['result']['userId'] == '2041199'
+        assert res.json()['data']['result']['uName'] == '157****0000'
+        assert re.match('KYYLT_2041199_', res.json()['data']['result']['token'])
 
     def test_login_fail(self):
         data = yaml.safe_load(open('D:/script/fastapp/data/login.yaml'))[1]
         res = LogIn().login(data)
         print(res.json())
+        assert res.status_code == 200
+        assert res.json()['retCode'] == 0
+        assert res.json()['isExpire'] == 1
+        assert res.json()['data']['code'] == 1003
+        assert re.search(r'\w+', res.json()['data']['message'])
