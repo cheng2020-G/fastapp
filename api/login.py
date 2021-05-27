7bd0ca6be2ef04859e4ae5878e11b7c83c4d9f60
cheng2020-G <1007884377@qq.com>
2021-05-27 10:38:24 +0800

    修改login_api

--- api/login.py
+++ api/login.py
@@ -5,12 +5,11 @@ from base.baseapi import BaseApi
 class LogIn:
     # 封装login接口方法，case调用此方法，data从case层传入
     def login(self, data_login):
-        self.http = BaseApi()
-        return self.http.http(data_login)
-
+        self.login = BaseApi()
+        return self.login.http(data_login)
 
 # if __name__ == '__main__':
 #     data_login_yaml = yaml.safe_load(open('D:/script/fastapp/data/login.yaml'))[0]
 #     print(data_login_yaml)
-#     login = LogIn().login(data_login_yaml)
-#     print(login.json())
+#     res = LogIn().login(data_login_yaml)
+#     print(res.json())
